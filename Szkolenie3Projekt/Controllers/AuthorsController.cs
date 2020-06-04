7be7e374b74d5c69b955566c7ef16b401d9d4bd0
Enaris <bularzkamil@gmail.com>
2020-06-04 11:06:52 +0200

    Controllers

---
+++ Szkolenie3Projekt/Controllers/AuthorsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Szkolenie3Projekt.DataAccess;
+using Szkolenie3Projekt.DataAccess.DbModels;
+using Szkolenie3Projekt.Services;
+
+namespace Szkolenie3Projekt.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private readonly IAuthorService _authorService;
+
+        public AuthorsController(IAuthorService authorService)
+        {
+            this._authorService = authorService;
+        }
+
+        // GET: Authors
+        public async Task<IActionResult> Index()
+        {
+            return View(await _authorService.GetAll());
+        }
+
+        // GET: Authors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            //var author = await _authorService.Get(id.Value);
+            var author = await _authorService.GetDeatils(id.Value);
+            
+            if (author == null)
+                return NotFound();
+
+            return View(author);
+        }
+
+        // GET: Authors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Authors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,DateOfBirth")] Author author)
+        {
+            if (!ModelState.IsValid)
+                return View(author);
+
+            var sameAuthor = await _authorService.Get(author.FirstName, author.LastName, author.DateOfBirth);
+            if (sameAuthor != null)
+            {
+                ModelState.AddModelError("", $"{author.FirstName} {author.LastName} born {author.DateOfBirth:dd/MM/yyy} already exists");
+                return View(author);
+            }
+
+            await _authorService.Create(author);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Authors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var author = await _authorService.Get(id.Value);
+
+            if (author == null)
+                return NotFound();
+            
+            return View(author);
+        }
+
+        // POST: Authors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,DateOfBirth")] Author author)
+        {
+            if (id != author.Id)
+                return NotFound();
+
+            if (!ModelState.IsValid)
+                return View(author);
+
+            var sameAuthor = await _authorService.Get(author.FirstName, author.LastName, author.DateOfBirth);
+            if (sameAuthor != null && sameAuthor.Id != id)
+            {
+                ModelState.AddModelError("", $"{author.FirstName} {author.LastName} born {author.DateOfBirth:dd/MM/yyy} already exists");
+                return View(author);
+            }
+
+            await _authorService.Update(author);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Authors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var author = await _authorService.Get(id.Value);
+
+            if (author == null)
+                return NotFound();
+
+            return View(author);
+        }
+
+        // POST: Authors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var result = await _authorService.Delete(id);
+            if (result == null)
+                return NotFound();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<bool> AuthorExists(int id)
+        {
+            var author = await _authorService.Get(id);
+            return author != null;
+        }
+    }
+}
---
+++ Szkolenie3Projekt/Controllers/BooksController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Szkolenie3Projekt.DataAccess;
+using Szkolenie3Projekt.DataAccess.DbModels;
+using Szkolenie3Projekt.DataAccess.Repositories;
+using Szkolenie3Projekt.Services;
+
+namespace Szkolenie3Projekt.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly IBookService _bookService;
+        private readonly IAuthorService _authorService;
+
+        public BooksController(IBookService bookService, IAuthorService authorService)
+        {
+            this._bookService = bookService;
+            this._authorService = authorService;
+        }
+
+        // GET: Books
+        public async Task<IActionResult> Index()
+        {
+            return View(await _bookService.GetAll());
+        }
+
+        // GET: Books/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var book = await _bookService.Get(id.Value);
+
+            if (book == null)
+                return NotFound();
+
+            return View(book);
+        }
+
+        // GET: Books/Create
+        public async Task<IActionResult> Create()
+        {
+            ViewData["AuthorId"] = new SelectList(await _authorService.GetAll(), "Id", "FirstName");
+            return View();
+        }
+
+        // POST: Books/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,ReleaseDate,Score,AuthorId")] Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData["AuthorId"] = new SelectList(await _authorService.GetAll(), "Id", "FirstName", book.AuthorId);
+                return View(book);
+            }
+
+            var sameBook = await _bookService.Get(book.Title, book.ReleaseDate, book.AuthorId);
+            if (sameBook != null)
+            {
+                ModelState.AddModelError("", $"{book.Title} released {book.ReleaseDate:dd/MM/yyy} already exists");
+                return View(book);
+            }
+
+            await _bookService.Create(book);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var book = await _bookService.Get(id.Value);
+
+            if (book == null)
+                return NotFound();
+
+            ViewData["AuthorId"] = new SelectList(await _authorService.GetAll(), "Id", "FirstName", book.AuthorId);
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,ReleaseDate,Score,AuthorId")] Book book)
+        {
+            if (id != book.Id)
+                return NotFound();
+
+            if (!ModelState.IsValid)
+                return View(book);
+
+            var sameBook = await _bookService.Get(book.Title, book.ReleaseDate, book.AuthorId);
+            if (sameBook != null && sameBook.Id != id)
+            {
+                ModelState.AddModelError("", $"{book.Title} released {book.ReleaseDate:dd/MM/yyy} already exists");
+                return View(book);
+            }
+
+            await _bookService.Update(book);
+
+            ViewData["AuthorId"] = new SelectList(await _authorService.GetAll(), "Id", "FirstName", book.AuthorId);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Books/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var book = await _bookService.Get(id.Value);
+
+            if (book == null)
+                return NotFound();
+
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var result = await _bookService.Delete(id);
+            if (result == null)
+                return NotFound();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<bool> BookExists(int id)
+        {
+            var book = await _bookService.Get(id);
+            return book!= null;
+        }
+    }
+}
