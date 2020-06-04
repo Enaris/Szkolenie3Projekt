1fedc61661da32427dbede48d355f7b581b35107
Enaris <bularzkamil@gmail.com>
2020-06-04 11:05:35 +0200

    Added automapper profiles

---
+++ Szkolenie3Projekt/Automapper/Profiles.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Szkolenie3Projekt.DataAccess.DbModels;
+using Szkolenie3Projekt.Services.DTOs;
+
+namespace Szkolenie3Projekt.Automapper
+{
+    public class Profiles : Profile
+    {
+        public Profiles()
+        {
+            CreateMap<Author, AuthorDetailsDto>();
+            CreateMap<Author, AuthorListDto>();
+            CreateMap<Book, AuthorBookDto>();
+            CreateMap<Book, BookListDto>();
+        }
+    }
+}
